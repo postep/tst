1adba664695e2beb61ee1373b9e17280859f2b89
Jakub Postępski <jakub.postepski@gmail.com>
2018-11-27 13:41:38 +0100

    x

--- projekt1/script.m
+++ projekt1/script.m
@@ -1,15 +1,16 @@
 %% Problem dynamiki w przestrzeni fazowej
 clear all;
 %% konstruowanie macierzy o zadanym widmie
-l1 = -1.5i;
-l2 = 0.5;
-v1 = [1, 0]';
-v2 = [0, 1]';
+l1 = 1.3*rand();
+l2 = -0.6 *rand();
+v1 = [-0.5, 3.3]'; 
+v2 = [1, 5]';
 
 v1scaled = v1./sqrt(sum(v1'*v1));
 v2scaled = v2./sqrt(sum(v2'*v2));
 
 V = [v1, v2];
+V = rand(2);
 D = [l1, 0; 0, l2];
 
 A = V*[l1 0; 0 l2]*(V^-1);
@@ -17,12 +18,12 @@ A = V*[l1 0; 0 l2]*(V^-1);
 f = @(x) A*x;
 
 %% konstruowanie zbioru punktow X0 rozlozonych na okregu 
-R = 1;
+R = 5;
 X0 = R*exp(1i * (0:pi/4:2*pi)); X0 = [real(X0); imag(X0)];
 T0 = length(X0);
 
 %% obliczanie trajektorii ukladu dla wybranych punktow poczatkowych
-T = 5;
+T = 20;
 for i=1:T0
     x(:, 1+(i-1)*T) = X0(:, i);
     for t = 1:T, x(:, t+1 + (i-1)*T) = f(x(:, t + (i-1)*T)); end
@@ -47,7 +48,7 @@ end;
 
 %% ilustracja obrazu Y = AX0
 
-Ximage = R*exp(1i * (0:pi/100:2*pi)); Ximage = [real(Ximage); imag(Ximage)];
+Ximage = R*exp(1i * (0:8*pi/100:2*pi)); Ximage = [real(Ximage); imag(Ximage)];
 Yimage = A*Ximage;
 
 plot(Ximage(1,:), Ximage(2,:), 'Color', 0.75*[0, 1, 0]);
@@ -83,6 +84,6 @@ axis equal;
 %% drukowanie
 set(fig, 'PaperUnits', 'centimeters');
 set(fig, 'PaperSize', [20 20]);
-figureName = ['imag_', num2str(l1), '_', num2str(l2)];
+figureName = ['demo2'];
 %figureName = 'demo';
 print(['images/', figureName, '.pdf'], '-dpdf', '-bestfit');
