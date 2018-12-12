8d67b830d624cbbb28a3212ea13d71cfd741120f
postep <jakub.postepski@gmail.com>
2018-12-12 22:31:30 +0100

    x

--- notatki/adaptacja.m
+++ notatki/adaptacja.m
@@ -1,34 +1,35 @@
-% g(t) = -ay(t) + bu(t)
-% u(t) = theta_1(t)u_c(t) - theta_2(t)y(t)
-gamma = 10.0;
-am = 10;
-bm = 1;
-dt = 1e-2;
+clear variables;
+dt = 1e-3;
+a = 1;
+b = 1;
+uc = 1;
+gamma = 1;
 
-ym = @(y, uc, am, bm) (-am*y + bm*uc)*dt;
-u = @(y, uc, theta_1, theta_2) theta_1*uc - theta_2*y;
+am = 100;
+bm = 10000;
 
-theta_1 = @(uc, e) -gamma*uc*e*dt;
-theta_2 = @(y, e) gamma*y*e*dt;
-
-% e = @(y, ym) y - ym;
 time = 0:dt:10;
 
-Y = zeros(size(time));
-YM = zeros(size(time));
-U = zeros(size(time));
-THETA_1 = zeros(size(time));
-THETA_2 = zeros(size(time));
-UC = 20*ones(size(time));
+u =zeros(size(time));
+uc = uc*ones(size(time));
+theta_1 = zeros(size(time));
+theta_2 = zeros(size(time));
+y = zeros(size(time));
+ym = zeros(size(time));
 
-for i = 2:length(time)
-    THETA_1(i) = theta_1(UC(i), Y(i-1)-YM(i-1));
-    THETA_2(i) = theta_2(Y(i), Y(i-1)-YM(i-1));
-    U(i) = u(Y(i-1), UC(i-1), THETA_1(i-1), THETA_2(i-1));
-    YM(i) = ym(YM(i-1), UC(i-1), am, bm);
-    Y(i) = ym(Y(i-1), U(i), THETA_1(i), THETA_2(i));
-end;
+for t = 2:length(time)
+    e = y(t-1)-ym(t-1);
+    theta_1(t) = -gamma*uc(t)*e*dt;
+    theta_2(t) = gamma*e*dt;
+    theta_1(t) = bm/b;
+    theta_2(t) = (am-a)/b;
+    u(t) = theta_1(t)*uc(t) - theta_2(t)*y(t-1);
+    y(t) = (-a*y(t-1) + b*u(t))*dt;
+    ym(t) = (-am*ym(t-1) + bm*uc(t))*dt;
+end
 
-%plot(time, THETA_1, time, THETA_2, time, Y, time, YM);
-%plot(time, U);
-plot(time, YM, time, Y);
+plot(time, y, time, ym);
+legend('Y', 'Ym');
+
+% plot(time, theta_1, time, theta_2, time, (bm/b)*ones(size(time)), time, ((am-a)/b)*ones(size(time)));
+% legend('theta_1', 'theta_2', 'exp_t1', 'exp_t2');
