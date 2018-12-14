3396680229711a8aa36236e4723349d5b03e02d1
postep <jakub.postepski@gmail.com>
2018-12-14 11:10:23 +0100

    x

--- notatki/adaptacja.m
+++ notatki/adaptacja.m
@@ -1,16 +1,25 @@
 clear variables;
 dt = 1e-3;
-a = 1;
-b = 1;
+a = 2;
+b = 2;
+am = 1;
+bm = 5;
 uc = 1;
-gamma = 1;
+gamma = 5000;
 
-am = 100;
-bm = 10000;
 
 time = 0:dt:10;
 
-u =zeros(size(time));
+sysd = c2d(ss([-a], [b], [1], [0]), dt);
+Ad = sysd.A;
+Bd = sysd.B;
+
+sysmd = c2d(ss([-am], [bm], [1], [0]), dt);
+Amd = sysmd.A;
+Bmd = sysmd.B;
+    
+
+u = zeros(size(time));
 uc = uc*ones(size(time));
 theta_1 = zeros(size(time));
 theta_2 = zeros(size(time));
@@ -21,15 +30,19 @@ for t = 2:length(time)
     e = y(t-1)-ym(t-1);
     theta_1(t) = -gamma*uc(t)*e*dt;
     theta_2(t) = gamma*e*dt;
-    theta_1(t) = bm/b;
-    theta_2(t) = (am-a)/b;
     u(t) = theta_1(t)*uc(t) - theta_2(t)*y(t-1);
-    y(t) = (-a*y(t-1) + b*u(t))*dt;
-    ym(t) = (-am*ym(t-1) + bm*uc(t))*dt;
+    y(t) = Ad*y(t-1) + Bd*u(t);
+    ym(t) = Amd*ym(t-1) + Bmd*uc(t);
 end
 
+subplot(2,1,1);
 plot(time, y, time, ym);
 legend('Y', 'Ym');
+title('Wyjscie modeli')
 
-% plot(time, theta_1, time, theta_2, time, (bm/b)*ones(size(time)), time, ((am-a)/b)*ones(size(time)));
-% legend('theta_1', 'theta_2', 'exp_t1', 'exp_t2');
+subplot(2,1,2);
+plot(time, theta_1, time, theta_2, time, (bm/b)*ones(size(time)), time, ((am-a)/b)*ones(size(time)));
+legend('t_1', 't_2', 'oczekiwane t_1', 'oczekiwane t_2');
+title('Parametry theta')
+
+
